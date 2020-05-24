427042eeadc8ca6c6b7bd69166d61d72928e2529
Яковлева Ольга Викторовна <OVYakovleva@alfabank.ru>
2020-05-24 03:48:40 +0300

    попытка пофиксить ошибку памяти #3

--- scripts/build_java.sh
+++ scripts/build_java.sh
@@ -1,7 +1,5 @@
 PATH_TO_BUILDGRADLE=$1
 
 chmod +x $PATH_TO_BUILDGRADLE/gradlew
-$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle unpack
-$PATH_TO_BUILDGRADLE/gradlew --stop
-$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle test
-$PATH_TO_BUILDGRADLE/gradlew --stop
+$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx256m -XX:MaxPermSize=64m" unpack
+$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx256m -XX:MaxPermSize=64m" test
