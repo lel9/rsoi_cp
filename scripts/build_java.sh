f6b83136cf8b68fb09c226db87762906b6bb4f29
Яковлева Ольга Викторовна <OVYakovleva@alfabank.ru>
2020-05-24 15:48:38 +0300

    fix build script

--- scripts/build_java.sh
+++ scripts/build_java.sh
@@ -2,4 +2,3 @@ PATH_TO_BUILDGRADLE=$1
 
 chmod +x $PATH_TO_BUILDGRADLE/gradlew
 $PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx256m -XX:MaxPermSize=64m" unpack
-$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx256m -XX:MaxPermSize=64m" test
