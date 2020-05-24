a9617b8bdef1f0258c60725ec90dcd2af2babd8b
Яковлева Ольга Викторовна <OVYakovleva@alfabank.ru>
2020-05-24 16:00:53 +0300

    уменьшен лимит памяти для тестов

--- scripts/build_java.sh
+++ scripts/build_java.sh
@@ -1,4 +1,4 @@
 PATH_TO_BUILDGRADLE=$1
 
 chmod +x $PATH_TO_BUILDGRADLE/gradlew
-$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx256m -XX:MaxPermSize=64m" unpack
+$PATH_TO_BUILDGRADLE/gradlew -b $PATH_TO_BUILDGRADLE/build.gradle -Dorg.gradle.jvmargs="-Xmx128m -XX:MaxPermSize=64m" unpack
