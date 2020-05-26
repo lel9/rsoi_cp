1a989371202f3d2e715d8d4ac9d584f5fdd12892
Яковлева Ольга Викторовна <OVYakovleva@alfabank.ru>
2020-05-26 04:01:52 +0300

    analyzer stub

--- patient_service/src/main/java/ru/bmstu/cp/rsoi/patient/config/WebConfig.java
+++ patient_service/src/main/java/ru/bmstu/cp/rsoi/patient/config/WebConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
